5e5b26eb1b2685044fdc1bc2c88a73ed7a1ca4d0
Neil Giarratana <neil@neilsmind.com>
2013-05-09 08:35:41 -0400

    added plus-sign to button

--- lib/active_admin/resource/action_items.rb
+++ lib/active_admin/resource/action_items.rb
@@ -54,7 +54,7 @@ module ActiveAdmin
         # New Link on all actions except :new and :show
         add_action_item :except => [:new, :show] do
           if controller.action_methods.include?('new') && authorized?(ActiveAdmin::Auth::CREATE, active_admin_config.resource_class)
-            link_to(I18n.t('active_admin.new_model', :model => active_admin_config.resource_label), new_resource_path, :class => "btn btn-large")
+            link_to("<i class='plus-sign'></i>".html_safe + I18n.t('active_admin.new_model', :model => active_admin_config.resource_label), new_resource_path, :class => "btn btn-large")
           end
         end
 
